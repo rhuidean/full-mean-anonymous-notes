f040f564a506462cb390bf9f9c60dc617eb76eb1
Jason <rhuidean06@gmail.com>
2017-07-14 18:18:06 -0500

    setup basic node, mongoose, and mongodb

---
+++ server/config/mongoose.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var fs = require('fs');
+
+mongoose.connect('mongodb://localhost/notes');
+mongoose.Promise = global.Promise;
+
+var models_path = __dirname + '/../models';
+
+fs.readdirSync(models_path).forEach(function(file){
+	if(file.indexOf('.js') != -1){
+		console.log('loading ' + file + '...');
+		require(models_path + '/' +file);
+	}
+});
+
+
